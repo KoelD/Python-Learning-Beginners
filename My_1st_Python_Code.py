77376fa23376bc2ee43766c5fa46743cc11c2982
Animesh Das <jobs4ani@gmail.com>
2023-04-07 00:30:06 +0530

    Added comments

--- My_1st_Python_Code.py
+++ My_1st_Python_Code.py
@@ -1,5 +1,4 @@
 name = input("Enter your name: ")
-
 full_marks = int(input("Full Marks Per Subject: "))
 while full_marks < 1:
     print(f"Full marks cannot be less than 1")
@@ -7,7 +6,8 @@ while full_marks < 1:
 
 # Define Subject List
 subjects = ['English', 'Maths', 'Social', 'Science', 'Hindi', "Computer Science"]
-# Define an empty dictionary
+
+# Define an empty dictionary to store marks of each subject
 marks = {}
 # define int var
 tot_marks = 0
@@ -19,13 +19,15 @@ for sub in subjects:
     marks[sub] = int(marks[sub])
     tot_marks += int(marks[sub])
 
+# Print Total subject & marks
 print(f"Total subject: {len(subjects)}")
 print(f"Total marks: {tot_marks}")
 
+# Calculate percentage scored
 percent = int(tot_marks / (len(subjects) * full_marks) * 100)
 print(f"Percentage scored : {percent}%")
 
-# My Grade Table
+# Define the grade table
 grade_range_percent = [
     {"grade": "A+", "s": 91, "e": 100},
     {"grade": "A", "s": 81, "e": 90},
@@ -35,12 +37,12 @@ grade_range_percent = [
     {"grade": "E", "s": 41, "e": 50},
     {"grade": "F", "s": 1, "e": 40}
 ]
-# Loop thru the list 
+# Loop thru the grade table list element 
 for gr_dic in grade_range_percent:
     # Print my grade table
     # print(f"Grade {gr_dic['grade']} for marks between {gr_dic['s']}% to {gr_dic['e']}%")
 
-    # Check by extracting the dict key value in range to compare with percent
+    # Check by element dict key value in range to compare with percent
     if int(gr_dic['s']) <= percent <= int(gr_dic['e']):
         print(f"{name} has scored grade : {gr_dic['grade']}")
 
